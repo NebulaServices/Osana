242ac32cf8da43bc25cbc09d8fb0c3bb80c54c22
Avad3 <65318266+Avad3@users.noreply.github.com>
2022-09-01 23:59:18 -0400

    Forgot to remove parsing comments from css.ts

--- src/lib/rewrite/css.ts
+++ src/lib/rewrite/css.ts
@@ -1,17 +1,8 @@
-// import { parse, walk, generate } from "css-tree";
 import rewriteURL from "./url";
 let originURL: any;
 
 export default function rewriteCSS (css: string, origin?: string): string {
   originURL = origin;
-  // const ast = parse(css);
-  // walk(ast, (node) => {
-  //   if (node.type === "Url") {
-  //     node.value = rewriteURL(node.value as any, origin) as any;
-  //   }
-  // });
-  // return generate(ast);
-  // /(?<=url\("?'?)[^"'][\S]*[^"'](?="?'?\);?)/g
 
   return css.replace(/(?<=url\("?'?)[^"'][\S]*[^"'](?="?'?\);?)/g, rewriteCSSURL);
 }
